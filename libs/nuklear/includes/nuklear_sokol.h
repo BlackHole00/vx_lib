8c853230695c22c9c05385d2ad9397a7999808f2
Vicix <lastapocalypse9@gmail.com>
2022-02-01 21:39:36 +0100

    Update nuklear_sokol

--- libs/nuklear/includes/nuklear_sokol.h
+++ libs/nuklear/includes/nuklear_sokol.h
@@ -219,44 +219,6 @@ typedef struct snk_desc_t {
     bool no_default_font;
 } snk_desc_t;
 
-typedef struct _snk_vertex_t {
-    float pos[2];
-    float uv[2];
-    uint8_t col[4];
-} _snk_vertex_t;
-
-typedef struct _snk_vs_params_t {
-    float   disp_size[2];
-    uint8_t _pad_8[8];
-} _snk_vs_params_t;
-
-typedef struct {
-    snk_desc_t desc;
-    struct nk_context ctx;
-    struct nk_font_atlas atlas;
-    _snk_vs_params_t vs_params;
-    size_t vertex_buffer_size;
-    size_t index_buffer_size;
-    sg_buffer vbuf;
-    sg_buffer ibuf;
-    sg_image img;
-    sg_shader shd;
-    sg_pipeline pip;
-    bool is_osx;    /* return true if running on OSX (or HTML5 OSX), needed for copy/paste */
-    #if !defined(SOKOL_NUKLEAR_NO_SOKOL_APP)
-    int mouse_pos[2];
-    float mouse_scroll[2];
-    bool mouse_did_move;
-    bool mouse_did_scroll;
-    bool btn_down[NK_BUTTON_MAX];
-    bool btn_up[NK_BUTTON_MAX];
-    char char_buffer[NK_INPUT_MAX];
-    bool keys_down[NK_KEY_MAX];
-    bool keys_up[NK_KEY_MAX];
-    #endif
-} _snk_state_t;
-_snk_state_t _snuklear;
-
 SOKOL_NUKLEAR_API_DECL void snk_setup(const snk_desc_t* desc);
 SOKOL_NUKLEAR_API_DECL struct nk_context* snk_new_frame(void);
 SOKOL_NUKLEAR_API_DECL void snk_render(int width, int height);
@@ -322,6 +284,44 @@ inline void snk_setup(const snk_desc_t& desc) { return snk_setup(&desc); }
 
 /* helper macros */
 #define _snk_def(val, def) (((val) == 0) ? (def) : (val))
+
+typedef struct _snk_vertex_t {
+    float pos[2];
+    float uv[2];
+    uint8_t col[4];
+} _snk_vertex_t;
+
+typedef struct _snk_vs_params_t {
+    float   disp_size[2];
+    uint8_t _pad_8[8];
+} _snk_vs_params_t;
+
+typedef struct {
+    snk_desc_t desc;
+    struct nk_context ctx;
+    struct nk_font_atlas atlas;
+    _snk_vs_params_t vs_params;
+    size_t vertex_buffer_size;
+    size_t index_buffer_size;
+    sg_buffer vbuf;
+    sg_buffer ibuf;
+    sg_image img;
+    sg_shader shd;
+    sg_pipeline pip;
+    bool is_osx;    /* return true if running on OSX (or HTML5 OSX), needed for copy/paste */
+    #if !defined(SOKOL_NUKLEAR_NO_SOKOL_APP)
+    int mouse_pos[2];
+    float mouse_scroll[2];
+    bool mouse_did_move;
+    bool mouse_did_scroll;
+    bool btn_down[NK_BUTTON_MAX];
+    bool btn_up[NK_BUTTON_MAX];
+    char char_buffer[NK_INPUT_MAX];
+    bool keys_down[NK_KEY_MAX];
+    bool keys_up[NK_KEY_MAX];
+    #endif
+} _snk_state_t;
+static _snk_state_t _snuklear;
 
 /*
     Embedded source code compiled with:
