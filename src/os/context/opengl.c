261e6bfb498d00a2100ebe9abe338e150698fb7a
Vicix <lastapocalypse9@gmail.com>
2022-02-05 21:48:58 +0100

    Using now core opengl 3.3. MacOS compatibility should be ok.

--- src/os/context/opengl.c
+++ src/os/context/opengl.c
@@ -4,7 +4,17 @@
 #include <glad/glad.h>
 #include <vx_utils.h>
 
+#include "../window.h"
+
 void vx_context_load_opengl(GLFWwindow* window) {
+    vx_glfw_window_hint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    vx_glfw_window_hint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    vx_glfw_window_hint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    vx_glfw_window_hint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     /*  Make the window's context current   */
     glfwMakeContextCurrent(window);
 
